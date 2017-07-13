a7ef65086e349d218c28db47c4e4974fea485562
e-serdyuk <drulsham@mail.ru>
2017-07-13 10:59:27 +0300

    Lesson 25 done

---
+++ React/lesson_25/HomeWork/2/src/actions/toDoActions.js
@@ -0,0 +1,23 @@
+﻿import dispatcher from '../dispatcher'; 
+
+// создать элемент 
+export function createItem(item) {
+    dispatcher.dispatch({
+        type: 'CREATE_ITEM', 
+        item
+    })
+}
+
+// удалить элемент
+export function removeItem(id) {
+    dispatcher.dispatch({
+        type: 'REMOVE_ITEM', 
+        id
+    })
+} 
+export function search(string) {
+    dispatcher.dispatch({
+        type:'SEARCH',
+        searchString:string
+    })
+}
