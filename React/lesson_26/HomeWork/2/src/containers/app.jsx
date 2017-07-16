64453325df7bbf9ec8cdd83f53e502948ca0c3d6
e-serdyuk <drulsham@mail.ru>
2017-07-16 15:21:10 +0300

    Lesson 26 homeWork 2

---
+++ React/lesson_26/HomeWork/2/src/containers/app.jsx
@@ -0,0 +1,64 @@
+﻿var React = require('react');
+var bindActionCreators =  require('redux').bindActionCreators;
+var connect = require('react-redux').connect;
+var actions = require('../actions/actions.jsx');
+var ListView = require('../components/itemList.jsx');
+var ItemTable = require('../components/itemTable.jsx');
+var EditBar = require('./editBar.jsx'); 
+
+class App extends React.Component {
+    constructor(){
+        super();
+   
+        this.choose =  this.choose.bind(this)
+            this.state = {
+            ch: ""
+         
+        } 
+        }
+
+            clickHandler(e) { 
+        this.props.deleteTask(e.target.dataset.id); 
+    }
+    choose(e) { 
+          this.setState({
+            ch: e.currentTarget.value
+        });
+     }
+
+    render() {
+    let chooseHandler = this.choose.bind(this);
+    let boundHandler = this.clickHandler.bind(this);
+        return (<div>
+            <div className="panel well">
+                <h2>Choose table or list</h2>
+           </div>
+           <select onChange={chooseHandler}>
+           <option value="table">table</option>
+           <option value="list">list</option>
+           </select>
+            <div className="panel well">
+                <h2>Tasks List</h2>
+           </div>
+            <div>
+                          {this.state.ch=="list" ? <ListView items={this.props.stateNew.users} handler={boundHandler}/> :          <ItemTable items={this.props.stateNew.users} handler={boundHandler}/>}
+            </div>
+            <EditBar />
+            </div>
+        )}
+}
+
+function mapStateToProps(state) {
+console.log(state)
+    return {
+        stateNew: state
+    }
+}
+
+function matchDispatchToProps(dispatch) {
+    return bindActionCreators({
+    deleteTask: actions.deleteTask
+     } , dispatch )
+}
+
+module.exports = connect(mapStateToProps, matchDispatchToProps)(App);
---
+++ React/lesson_26/HomeWork/2/src/reducers/reducer.jsx
@@ -0,0 +1,54 @@
+﻿const reducer = (state={users: null}, action) => {
+
+    switch(action.type) {
+        case 'CREATE_TASK': { 
+
+            var newUser = action.payload; 
+            if(action.payload.name!=="")
+            var newState = state.users.concat(action.payload) 
+        else {alert("Enter the name")
+         newState=state.users;
+         }
+
+            return {...state, users:newState} 
+            break; 
+        } 
+      
+        case 'DELETE_TASK': {
+            var id = action.payload 
+
+            function matchesId(val) {
+                if (val.id != id) {
+                    return val
+                }
+            }
+            var newData = state.users.filter(matchesId);
+
+            return {...state, users: newData} 
+            break;
+        }
+        case 'FETCH_USERS_START': {
+            return {...state, fetching: true, users: []}
+            break;
+        }
+        case 'FETCH_USERS_ERROR': {
+            return {...state, fetching: false, error: action.payloads, users: []}
+            break;
+        }
+        case 'RECEIVE_USERS': {
+            return {
+                ...state,
+                fetching: false,
+                fetched: true,
+                users: action.payload
+            }
+            break;
+        }
+
+        default: {
+            return state;
+        }
+    }
+};
+
+module.exports = reducer;
